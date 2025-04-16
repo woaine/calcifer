1fbb8037082600f1e993b88727a3b388f5504e56
Joy Aine Etom <ejoyaine@gmail.com>
2025-04-17 03:23:37 +0800

    chore: add outlier analysis import

--- src/eda/__init__.py
+++ src/eda/__init__.py
@@ -1,3 +1,4 @@
 from eda.distribution_analysis import dataframe_summary
 from eda.distribution_analysis import distribution_analysis
 from eda.correlation_analysis import correlation_analysis
+from eda.outlier_analysis import outlier_analysis
